8e33f1a6d3b90b55a9bd4f92df1c9b81883896d1
Rohan Pandav <rohan.pandav36@svkmmumbai.onmicrosoft.com>
2021-04-26 04:04:51 +0530

    added date and time picker

--- app/src/main/java/com/example/ezmanagement/lecture_fac.java
+++ app/src/main/java/com/example/ezmanagement/lecture_fac.java
@@ -2,25 +2,84 @@ package com.example.ezmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class lecture_fac extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     String[] courses = {"Mobile Application Development", "Data structures",
             "Software Engineering", "Artificial Intelligence",
             "Probability And Statistics", "Image Processing", "Python Programming", "Image Processing", "Java Programming"};
     Spinner spin;
+    TextView dateFormat,TimeTextview;
+    private TextView pickTime;
+    int year;
+    int month;
+    int day;
     Button atn_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecture_fac);
         spin = findViewById(R.id.spinner_lec_fac);
+        dateFormat = findViewById(R.id.dateFormatID);
+        TimeTextview = findViewById(R.id.pickTime);
+        pickTime = findViewById(R.id.pickTime);
+        pickTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int hours=calendar.get(Calendar.HOUR_OF_DAY);
+                int minutes=calendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(lecture_fac.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Calendar c = Calendar.getInstance();
+                        c.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        c.set(Calendar.MINUTE,minute);
+                        c.setTimeZone(TimeZone.getDefault());
+                        SimpleDateFormat format;
+                        format = new SimpleDateFormat("k:mm a");
+                        String time = format.format(c.getTime());
+                        TimeTextview.setText(time);
+                    }
+                },hours,minutes, false);
+                timePickerDialog.show();
+            }
+        });
+
+
+        Calendar calendar = Calendar.getInstance();
+        dateFormat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(lecture_fac.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        dateFormat.setText(SimpleDateFormat.getDateInstance().format(calendar.getTime()));
+
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
         atn_btn = findViewById(R.id.lecture_btn);
         spin.setOnItemSelectedListener(this);
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,courses);

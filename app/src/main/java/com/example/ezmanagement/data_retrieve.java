b96a89d0d7e6e0412492c42783bf46ed24f399a4
Amber Upadhyay <ambersupadhyay@gmail.com>
2021-04-26 01:51:56 +0530

    login authentication with new intent to retrieve data

---
+++ app/src/main/java/com/example/ezmanagement/data_retrieve.java
@@ -0,0 +1,14 @@
+package com.example.ezmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class data_retrieve extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_retrieve);
+    }
+}

1bfb2a1e11fdc3204f5495ffd00ee42056b67b0e
Rohan Pandav <rohan.pandav36@svkmmumbai.onmicrosoft.com>
2021-04-27 02:40:10 +0530

    student datepicker + spinner fixes

--- app/src/main/java/com/example/ezmanagement/attendance_stu.java
+++ app/src/main/java/com/example/ezmanagement/attendance_stu.java
@@ -2,15 +2,19 @@ package com.example.ezmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class attendance_stu extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     String[] courses = {"Mobile Application Development", "Data structures",
@@ -18,20 +22,46 @@ public class attendance_stu extends AppCompatActivity implements AdapterView.OnI
             "Probability And Statistics", "Image Processing", "Python Programming", "Image Processing", "Java Programming"};
     Spinner spin;
     Button atn_btn;
+    int year;
+    int month;
+    int day;
+    TextView dateFormat;
+    public String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_stu);
-    spin = findViewById(R.id.spinner_atn_stu);
-    atn_btn = findViewById(R.id.chk_atn_btn);
-    spin.setOnItemSelectedListener(this);
-    ArrayAdapter arrayAdapter = new ArrayAdapter(this,R.layout.spinner_item,courses);
-    arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    spin.setAdapter(arrayAdapter);
+        spin = findViewById(R.id.spinner_atn_stu);
+        atn_btn = findViewById(R.id.chk_atn_btn);
+        spin.setOnItemSelectedListener(this);
+        dateFormat = findViewById(R.id.dateFormatID4);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.spinner_item, courses);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(arrayAdapter);
 
+        Calendar calendar = Calendar.getInstance();
+        dateFormat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(attendance_stu.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
+                        int m = calendar.get(Calendar.MONTH);
+                        m++;
 
+                        date = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)) + "-" + String.valueOf(m) + "-" +
+                                String.valueOf(calendar.get(Calendar.YEAR));
+                        dateFormat.setText(date);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
 
     @Override
